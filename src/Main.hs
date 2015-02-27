7ba8859ad5fea71922a0e2247634e867ce48927e
Ryan Nash <nash.rn@gmail.com>
2015-02-27 10:29:48 -0500

    fix

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,33 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
-import Control.Lens
-import Data.Aeson.Lens (key)
-import Network.Wreq
+import Control.Lens ((.~), (&), (^.), _head, (^?), filtered)
+import Data.Aeson (Value(..))
+import Data.Aeson.Lens (key, _String, _Array)
+import Network.Wreq (get, getWith, defaults, header, Response, asJSON, responseBody)
+import Data.Map (Map)
+import qualified Data.Vector as V
 
-url = "https://circleci.com/api/v1/project/zephyr-dev/gust/9138?circle-token=a5422c509e6c049514733030174a901e8cd17b3e"
+url = "https://circleci.com/api/v1/project/zephyr-dev/gust?circle-token=a5422c509e6c049514733030174a901e8cd17b3e"
 
-type Resp = Response (Map String Value)
+{- type Build = Value -}
+{- type Body = [Build] -}
+type Body = Value
+
+opts = defaults & header "Accept" .~ ["application/json"]
 
 main :: IO ()
 main = do
-  r <- asJSON =<< get url :: IO Resp
-  putStrLn $ r ^? responseBody . key "steps"
+  (r :: Response Body) <- asJSON =<< getWith opts url
+  let builds = r ^. responseBody . _Array -- . filtered (\b -> (b ^? key "status" . _String) == "running") 
+  print $ V.length $ V.filter (\b -> b ^. key "status" . _String == "running") builds  -- == "running"
 
+  {- putStrLn $ show $ builds -}
+
+  {- where -}
+    {- filterRunning :: [Build] -> [Build] -}
+    {- filterRunning = filter running -}
+
+    {- running :: Build -> Bool -}
+    {- running b = b ^. key "status" . _String == "running" -}
 
